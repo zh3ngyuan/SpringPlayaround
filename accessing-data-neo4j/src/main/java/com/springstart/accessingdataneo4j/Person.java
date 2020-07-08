d2d3ac1faf66311ec19deb85ac1f24a578b9dfec
ALexander4295502 <yuanzhengstl@gmail.com>
2020-07-07 23:14:19 -0700

    Create an app for accessing data using neo4j

---
+++ accessing-data-neo4j/src/main/java/com/springstart/accessingdataneo4j/Person.java
@@ -0,0 +1,55 @@
+package com.springstart.accessingdataneo4j;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NodeEntity
+public class Person {
+    @Id @GeneratedValue private long id;
+
+    private String name;
+
+    private Person() {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Relationship(type = "TEAMMATE", direction = Relationship.UNDIRECTED)
+    public Set<Person> teammates;
+
+    public void worksWith(Person person) {
+        if (teammates == null) {
+            teammates = new HashSet<>();
+        }
+        teammates.add(person);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return String.format(
+                "%s's teammates => %s",
+                this.name,
+                Optional.ofNullable(this.teammates)
+                        .orElse(Collections.emptySet())
+                        .stream()
+                        .map(Person::getName)
+                        .collect(Collectors.toList())
+        );
+    }
+}
