c4c588ffb38d0ec9b3dbd228a11842ecfcd71143
ALexander4295502 <yuanzhengstl@gmail.com>
2020-07-09 22:46:37 -0700

    Create an app for creating a Batch Service

---
+++ batch-processing/src/test/java/com/springstart/batchprocessing/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
