cca6525279a47d8cfcc40f7ce5f5bb91dd0eb7bb
ALexander4295502 <yuanzhengstl@gmail.com>
2020-07-10 21:48:26 -0700

    Create an app for building a Hypermedia-Driven RESTful Web Service

---
+++ rest-hateoas/src/test/java/com/springstart/resthateoas/RestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.resthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
